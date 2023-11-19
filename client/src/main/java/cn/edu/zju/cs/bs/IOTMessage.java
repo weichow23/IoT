39b4c0865738d43f8d48c32e98fe5b1049e5b6f0
Wesley Chou <3210103790@zju.edu.cn>
2023-11-19 21:49:20 +0800

    last client

--- client/src/main/java/cn/edu/zju/cs/bs/IOTMessage.java
+++ client/src/main/java/cn/edu/zju/cs/bs/IOTMessage.java
@@ -1,26 +1,14 @@
 package cn.edu.zju.cs.bs;
+import lombok.Data;
 
 public class IOTMessage {
 
-    // 设备ID
-    private String clientId;
-
-    // 上报信息
+    private String clientId; // deviceID
     private String info;
-
-    //设备数据
     private int value;
-
-    //是否告警，0-正常，1-告警
-    private int alert;
-
-    //设备位置，经度
+    private int alert; // 0: safe 1: at risk
     private double lng;
-
-    //设备位置，纬度
     private double lat;
-
-    //上报时间，ms
     private long timestamp;
 
     public String getClientId() {
